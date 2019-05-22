4944317ef7bb3f27be71481bf56b7e9255f8429d
donatociao <ciao.donatociao@gmail.com>
2019-05-23 01:52:02 +0200

    OK

--- main.js
+++ main.js
@@ -6,37 +6,43 @@
 // Inizialmente chiamate l'API con un numero fisso stabilito da voi,
 // in modo da interrogare l'API nel modo corretto e stilare le card.
 // Poi aggiungete la richiesta del numero dei giocatori all'utente
-
-
-
-
 $(document).ready(function(){
+//chiedo all'utente quanti giocatori vuole generare
+  var n = prompt('Quanti giocatori vuoi generare?');
 
-  // var n = prompt('Quanti giocatori vuoi generare?');
-
+  //interrogo API
   $.ajax({
     url: "https://www.boolean.careers/api/array/basket",
     type: "GET",
     data: {
-      'n': 3
+      //Imposto valori interrogazione
+      'n': n
     },
     success: function(risposta){
+      //imposto variabile con la risposta generata
       var giocatori = risposta.response;
-      console.log(giocatori[0])
-      var ok = $('#template').html();
-      var template = Handlebars.compile(ok);
+      //indico il codice da clonare
+      var codetoclone = $('#template').html();
+      //imposto funzione compilatore
+      var template = Handlebars.compile(codetoclone);
+      //dichiaro variabile che definisce i valori da cercare per creare un giocatore
+      var player;
+      //dichiaro variabile per appendere il codice generato
+      var html;
 
-      var player = {
-        'code': giocatori[0].playerCode,
-        'points': giocatori[0].points,
-        'fouls': giocatori[0].fouls,
-        'rebounds': giocatori[0].rebounds,
-        'two': giocatori[0].twoPoints,
-        'three': giocatori[0].threePoints
-      };
-      var html = template(player) ;
-      $('.container').append(html)
-
+      //inizializzo un ciclo per generare i giocatori desiderati
+      for (i = 0; i < n; ++i) {
+        player = {
+          'code': giocatori[i].playerCode,
+          'points': giocatori[i].points,
+          'fouls': giocatori[i].fouls,
+          'rebounds': giocatori[i].rebounds,
+          'two': giocatori[i].twoPoints,
+          'three': giocatori[i].threePoints
+        };
+        html = template(player) ;
+        $('.container').append(html)
+      }
     },
     error: function(){
       alert("Chiamata fallita!!!");
